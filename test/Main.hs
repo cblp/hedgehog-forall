b21e3e8ce190a2913b4d462a11c77c59fb1c24fb
Yuriy Syrovetskiy <cblp@cblp.su>
2019-08-28 17:13:11 +0300

    Add semigroup laws

--- test/Main.hs
+++ test/Main.hs
@@ -1,2 +1,10 @@
+import Hedgehog.Classes (lawsCheck, semigroupLaws)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
 main :: IO ()
-main = pure ()
+main = do
+  True <-
+    lawsCheck $ semigroupLaws $ Gen.string (Range.linear 0 100) Gen.unicode
+  True <- lawsCheck $ semigroupLaws $ Gen.text (Range.linear 0 100) Gen.unicode
+  pure ()
